d777e3ead67463b95e703eeac6f4e9ae3948dcdc
Martin Odongo <martinwere14@gmail.com>
2024-10-04 15:24:11 +0300

    js

---
+++  quiz.js
@@ -0,0 +1,8 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+userAnswer=document.querySelector("quiz").value
+
+if(value===correctAnswer){
+userAnswer.textContent="Correct! Well done."
+}
+}
