8f750124ec7d2a0c657d88b0ea6d994565fe08e7
Martin Odongo <martinwere14@gmail.com>
2024-10-04 15:37:49 +0300

    js

--- quiz.js
+++ quiz.js
@@ -1,8 +1,15 @@
 function checkAnswer(){
     const correctAnswer = "4";
 userAnswer=document.querySelector("quiz").value
+const answerCheckbox = document.getElementById("choice1");
+
 
 if(value===correctAnswer){
 userAnswer.textContent="Correct! Well done."
+answerCheckbox.checked = true;
+}
+else{
+    userAnswer.textContent= "That's incorrect. Try again!".
+    answerCheckedbox.checked = false;
 }
 }
