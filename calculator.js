e9c14381a9c5ea1d6fe12b5b4630f828c2d96ab3
Martin Odongo <martinwere14@gmail.com>
2024-10-04 15:07:26 +0300

    made a quiz files

--- calculator.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('add').addEventListener('click', function() {
-    const number1 = parseFloat(document.getElementById('number1').value) || 0;
-    const number2 = parseFloat(document.getElementById('number2').value) || 0;
-    // return number1 + number2
-    const result = add(number1, number2);
-    document.getElementById('calculation-result').textContent = result;
-});
